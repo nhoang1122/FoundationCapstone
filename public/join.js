18a42c5ee089cf7de10ff78ad79fc23bc8a2df2e
Nhat Hoang <johnhoanghtx@gmail.com>
2022-06-21 14:33:08 -0500

    Edit JS

--- public/join.js
+++ public/join.js
@@ -1,7 +1,7 @@
 const joinBtn = document.querySelector('#btn-join');
 
 const alertJoin = () => {
-    alert("Check Email for Confirmation!")
+    alert("Thank you for your subscription. Process may take up to 10 minutes. Check Email for Confirmation!")
 }
 
 joinBtn.addEventListener('click', alertJoin)
